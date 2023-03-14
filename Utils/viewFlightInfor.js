e45a8817263e9ad109e269e35627ff2ae9fae716
vuongbachdoan <vuongbachdoan@gmail.com>
2023-03-14 10:12:15 +0700

    feat(vuong): add view information of flight

--- Utils/viewFlightInfor.js
+++ Utils/viewFlightInfor.js
@@ -1,11 +1,22 @@
 var selectedTicket = null;
+var isInforOpen = false;
 
 function viewInfor(index) {
-    if (selectedTicket === null) {
-        selectedTicket = document.getElementsByClassName("ticket_detail")[index];
-        selectedTicket.style.display = "flex"
-    } else {
-        selectedTicket.style.display = "none";
-        selectedTicket = null;
-    }
+    let currentTicket = document.getElementsByClassName("ticket_infor_detail")[index];
+    currentTicket.style.display = "none";
+    selectedTicket = document.getElementsByClassName("ticket_detail")[index];
+    selectedTicket.style.display = "flex";
 }
+
+function viewFlightInfor(index) {
+    let currentTicket = document.getElementsByClassName("ticket_detail")[index];
+    currentTicket.style.display = "none";
+    selectedTicket = document.getElementsByClassName("ticket_infor_detail")[index];
+    selectedTicket.style.display = "flex";
+}
+
+window.addEventListener("click", (e) => {
+    if ((e.target).contains(selectedTicket)){
+        selectedTicket.style.display = "none";
+    }
+})
